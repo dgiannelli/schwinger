0011cf367f5da8eb8386230d3a052d1b8d89ac74
Giovanni Iannelli <iannelli.giovanni@gmail.com>
2016-08-19 19:20:55 +0200

    Finished pure gauge + error

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 CC = gcc
 CFLAGS = -O3 -lm
 
-schwinger: schwinger.exe
+schwingerPlot: schwinger clover.py
+	python clover.py
+
+schwinger: schwinger.exe blocking.exe
 	./schwinger.exe
+	./blocking.exe clover.dat
 
 schwinger.exe: schwinger.o lattice.o
 	$(CC) $(CFLAGS) -o schwinger.exe schwinger.o lattice.o
@@ -13,6 +17,9 @@ schwinger.o: schwinger.c lattice.h
 lattice.o: lattice.c lattice.h
 	$(CC) $(CFLAGS) -c -o lattice.o lattice.c
 
+blocking.exe: blocking.c
+	$(CC) $(CFLAGS) -o blocking.exe blocking.c
+
 clean:
-	rm *.exe *.o *.dat
+	rm *.exe *.o *.dat *.pdf
 
--- schwinger.c
+++ schwinger.c
@@ -3,9 +3,9 @@
 
 #include "lattice.h"
 
-#define N 10 //Lattice size
-#define IMAX 100 //Total iterations
-#define ITHERM 20 //Number of iters needed to thermalize
+#define N 20 //Lattice size
+#define IMAX 1000 //Number of measurement
+#define ITHERM 10 //Number of thermalization iteration
 
 #define BETA 1. //Action beta parameter
 
@@ -18,12 +18,15 @@ int main(int argc, char *argv[])
 
     FILE *cloverFile = fopen("clover.dat", "w");
 
-    for (i=0; i<IMAX; i++)
+    for (i=0; i<ITHERM+IMAX; i++)
     {
         SweepLattice(lattice, BETA, N);
-        const double clover = GetRandomClover(lattice, N);
-        fprintf(cloverFile, "%e\n", 1. - clover);
-        if (i>=ITHERM) plaquetteSum += clover;
+        if (i>=ITHERM) 
+        {
+            const double clover = GetRandomClover(lattice, N);
+            fprintf(cloverFile, "%.10e\n", 1. - clover);
+            plaquetteSum += clover;
+        }
     }
 
     fclose(cloverFile);

45d47d1488598e68fc816b087235270cb30587e7
Giovanni Iannelli <iannelli.giovanni@gmail.com>
2016-09-08 15:43:29 +0200

    added jackknife.py

---
+++ jackknife.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def f(x): return sum(x**2)
+
+def JackknifeMean(x):
+    n = x.size
+    idx = np.arange(n)
+    return np.sum(f(x[idx!=i]) for i in range(n))/float(n)
+
+def Jackknife(x):
+    n = x.size
+    idx = np.arange(n)
+    jMean = JackknifeMean(x)
+    return (n-1)/float(n)*np.sum( (f(x[idx!=i])-jMean)** 2 for i in range(n) )
+

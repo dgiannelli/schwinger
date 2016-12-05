891af714b204fcf26d6c20eea5d4b378165d5ec5
dgiannelli <iannelli.giovanni@gmail.com>
2016-12-05 13:41:21 +0100

    finished analysis plaquette

--- analysis.c
+++ analysis.c
@@ -1,48 +1,82 @@
-#include <stdio.h> #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <strings.h>
 #include <assert.h>
+#include <gsl/gsl_math.h>
 
 #define THERMRATIO 0.2
 #define NBINS 20
 
 double beta;
 int n;
+char bounds[10];
 int sweeps;
 int getPlaquette = 0;
 char plaquetteHistory[40];
 char plaquetteAnalysis[40];
 
+void Bunching(FILE *file)
+{
+    const int therm = sweeps*THERMRATIO;
+    const int size = sweeps - therm;
+    const int binSize = ( size + NBINS - 1 ) / NBINS;
+
+    for (int i=0; i<therm; i++) fscanf(file, "%*lf");
+
+    double mean = 0.0;
+    double meanSq = 0.0;
+    for (int bin=0; i<NBINS; i++)
+    {
+        int i;
+        for(i=0; i<binSize && fscanf(file, "%lf", &temp); i++)
+        {
+            meanBin += temp;
+        }
+        meanBin /= i;
+        mean += meanBin;
+        meanSq += gls_pow_2(meanBin);
+    }
+    mean /= NBINS;
+    meanSq /= NBINS;
+
+    const double error = sqrt( (meanSq-gsl_pow_2(mean)) / (NBINS - 1) ); 
+
+    fprintf(file, "%.16e\t%.16e\t%.1f\t%i\t%s\n", mean, error, beta, n, bounds);
+}
+
 int main(int argc, char *argv[])
 {
     assert(argc==2);
-
 
     FILE *paramFile = fopen(argv[1], "r");
     char paramName[40], paramValue[40];
     while ( fscanf(paramFile, "%s %s", &paramName, &paramValue) == 2 )
     {
-        if (!strcmp(paramName,"beta")) beta = atof(paramValue);
+        if      (!strcmp(paramName,"beta")) beta = atof(paramValue);
         else if (!strcmp(paramName,"n")) n = atoi(paramValue);
+        else if (!strcmp(paramName,"bounds")) strcpy(bounds,paramValue);
         else if (!strcmp(paramName,"sweeps")) sweeps = atoi(paramValue);
         else if (!strcmp(paramName,"getPlaquette")) getPlaquette = atoi(paramValue);
         else if (!strcmp(paramName,"plaquetteHistory")) strcpy(plaquetteHistory,paramValue);
         else if (!strcmp(paramName,"plaquetteAnalysis")) strcpy(plaquetteAnalysis,paramValue);
     }
 
-    FILE *plaquetteHistoryFile, plaquetteAnalysisFile;
+    FILE *plaquetteHistoryFile, *plaquetteAnalysisFile;
     if (getPlaquette)
     {
         assert(plaquetteHistoryFile = fopen(plaquetteHistory, "r"));
-        if (!plaquetteAnalysis) assert(plaquetteAnalysisFile = fopen(plaquetteAnalysis, "w"));
+        if (plaquetteAnalysis) assert(plaquetteAnalysisFile = fopen(plaquetteAnalysis, "a"));
         else plaquetteAnalysisFile = stdout;
+
+        Bunching(plaquetteAnalysisFile);
+
+        if (plaquetteAnalysis) fclose(plaquetteAnalysisFile);
     }
 
-    const int therm = sweeps*THERMRATIO;
-    const int size = sweeps - therm;
-    const int binSize = ( size + NBINS - 1 ) / NBINS;
-    for (int i=0; i<therm; i++) fscanf(plaquetteHistoryFile, "%*lf");
-    for (int bin=0; i<NBINS; i++)
-    {
-        for(int i=0; i<binSize && fscanf(
+    return 0;
+}
+
+
+
 
 
